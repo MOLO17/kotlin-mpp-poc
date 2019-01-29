de0bfe4b9d91d745389dad511806592b39ff3498
Damiano Giusti <damiano.giusti@molo17.com>
2019-01-29 21:59:17 +0100

    Cleanup view controller

--- ios-app/Kotlin-MPP/Kotlin-MPP/ViewController.swift
+++ ios-app/Kotlin-MPP/Kotlin-MPP/ViewController.swift
@@ -12,26 +12,34 @@ import CoreBluetooth
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var label: UITextView!
     
-    private var adapter: BluetoothAdapter!
+    private var adapter: BluetoothAdapter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = MainKt.getMessage()
-        adapter = BluetoothAdapter { adapter -> KotlinUnit in
-            adapter.discoverDevices(callback: self.didReceiveDevices(devices:))
-            return KotlinUnit()
-        }
-
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        adapter = BluetoothAdapter { [weak self] adapter -> KotlinUnit in
+            if let callback = self?.didReceiveDevices(devices:) {
+                adapter.discoverDevices(callback: callback)
+            }
+            return KotlinUnit()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        adapter?.stopScan()
+        adapter = nil
+    }
 
     private func didReceiveDevices(devices: [BluetoothDevice]) -> KotlinUnit {
         label.text = devices.map { "ID: \($0.id)\nName: \($0.name)" }.joined(separator: "\n\n")
         return KotlinUnit()
     }
-
 }
 
