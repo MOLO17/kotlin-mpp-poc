93164663e568fcd7c6a473ba8d9fd394cf4c0795
Damiano Giusti <damiano.giusti@molo17.com>
2019-01-29 11:33:36 +0100

    Create project

---
+++ app/src/main/java/com/molo17/damianogiusti/kotlinnativepoc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.molo17.damianogiusti.kotlinnativepoc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
