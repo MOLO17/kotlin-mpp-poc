26181b5b719dd5ad65eaa2b577b9ee2d54d4ba30
Matteo Sist <matteo.sist@molo17.com>
2019-01-30 16:05:02 +0100

    android implementation

--- shared/src/androidMain/kotlin/com/molo17/damianogiusti/platformName.kt
+++ shared/src/androidMain/kotlin/com/molo17/damianogiusti/platformName.kt
@@ -1,16 +1,36 @@
 package com.molo17.damianogiusti
 
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.os.Build
+import android.bluetooth.BluetoothAdapter as AndroidBluetoothAdapter
 
 actual fun platformName(): String {
     return "Android ${Build.VERSION.RELEASE}"
 }
 
-actual class BluetoothAdapter actual constructor(whenReady: (BluetoothAdapter) -> Unit) {
+actual class BluetoothAdapter actual constructor(whenReady: (BluetoothAdapter) -> Unit): ScanCallback() {
+
+    private val bluetoothAdapter by lazy { AndroidBluetoothAdapter.getDefaultAdapter() }
+
+    private var callback: ((List<BluetoothDevice>) -> Unit)? = null
+
     actual fun discoverDevices(callback: (List<BluetoothDevice>) -> Unit) {
+        this.callback = callback
+
+        if (bluetoothAdapter.isDiscovering) {
+            bluetoothAdapter.cancelDiscovery()
+        }
+
+        bluetoothAdapter.bluetoothLeScanner.startScan(this)
     }
 
     actual fun stopScan() {
+        bluetoothAdapter.bluetoothLeScanner.stopScan(this)
+    }
+
+    override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+        results?.map { it.device }?.map { BluetoothDevice(it.address, it.name) }?.let { callback?.invoke(it) }
     }
 
 }
