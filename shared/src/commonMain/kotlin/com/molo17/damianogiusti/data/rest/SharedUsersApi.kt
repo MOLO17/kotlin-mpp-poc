60d33f5e26b6fae68f3ad01cfa80a8b64cbf3050
Damiano Giusti <damiano.giusti@molo17.com>
2019-02-01 18:08:39 +0100

    Use Ktor auto deserialization from Json

--- shared/src/commonMain/kotlin/com/molo17/damianogiusti/data/rest/SharedUsersApi.kt
+++ shared/src/commonMain/kotlin/com/molo17/damianogiusti/data/rest/SharedUsersApi.kt
@@ -1,18 +1,23 @@
 package com.molo17.damianogiusti.data.rest
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import kotlinx.serialization.json.Json
 
 /**
  * Created by Damiano Giusti on 01/02/19.
  */
 class SharedUsersApi : UsersApi {
 
-    private val httpClient = HttpClient()
-
-    override suspend fun getUsers(): AllUsersResponseDto {
-        val json = httpClient.get<String>("https://randomuser.me/api/?results=20")
-        return Json.parse(AllUsersResponseDto.serializer(), json)
+    private val httpClient = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer().apply {
+                setMapper(AllUsersResponseDto::class, AllUsersResponseDto.serializer())
+            }
+        }
     }
+
+    override suspend fun getUsers(): AllUsersResponseDto =
+        httpClient.get("https://randomuser.me/api/?results=50")
 }
