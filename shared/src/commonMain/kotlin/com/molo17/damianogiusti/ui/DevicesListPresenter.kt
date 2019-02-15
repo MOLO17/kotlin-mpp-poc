5aad379981fac3fb0fbb1e8d54fade0b87229c4c
Damiano Giusti <damiano.giusti@molo17.com>
2019-02-15 13:00:33 +0100

    Update presenter

--- shared/src/commonMain/kotlin/com/molo17/damianogiusti/ui/DevicesListPresenter.kt
+++ shared/src/commonMain/kotlin/com/molo17/damianogiusti/ui/DevicesListPresenter.kt
@@ -1,30 +1,76 @@
 package com.molo17.damianogiusti.ui
 
-import com.molo17.damianogiusti.BluetoothAdapter
-import com.molo17.damianogiusti.BluetoothDevice
+import com.molo17.damianogiusti.*
+
+private const val SERVICE_BUTTON_PRESSED = "FEE0"
+private const val CHAR_BUTTON_PRESSED = "00000010"
+private const val SERVICE_HEART_RATE = "180D"
+private const val CHAR_HEART_RATE = "2A37"
 
 /**
  * Created by Damiano Giusti on 01/02/19.
  */
 class DevicesListPresenter(
     private val bluetoothAdapter: BluetoothAdapter
-) {
+) : BluetoothAdapterListener {
+
     private var view: DevicesListView? = null
-    private var discoveredDevices = mutableMapOf<String, BluetoothDevice>()
+
+    private var characteristic: BleCharacteristic? = null
 
     fun attachView(v: DevicesListView) {
         view = v
-
-        bluetoothAdapter.discoverDevices { device ->
-            discoveredDevices[device.id] = device
-            val uiDevices = discoveredDevices.values.map(::mapToUiDevice)
+        bluetoothAdapter.listener = this
+        bluetoothAdapter.findBondedDevices { devices ->
+            val uiDevices = devices.map(::mapToUiDevice)
             view?.showDevices(uiDevices)
+
+            val device = devices.find { it.name.startsWith("MI") }
+            if (device == null) {
+                view?.showMessage("MI Band not bonded to your ${platformName()} device.")
+            } else {
+                view?.showMessage("Connecting device ${device.id}")
+                bluetoothAdapter.connect(device)
+            }
         }
     }
 
     fun detachView() {
-        bluetoothAdapter.stopScan()
+        characteristic?.let { char ->
+            bluetoothAdapter.setNotificationDisabled(char)
+            bluetoothAdapter.disconnect()
+            bluetoothAdapter.listener = null
+        }
+        characteristic = null
+        view?.showDevices(emptyList())
         view = null
+    }
+
+    override fun onStateChange(state: BleState) {
+        when (state) {
+            is BleState.Connected -> {
+                view?.showMessage("Connected device ${state.device.name}")
+                bluetoothAdapter.discoverServices()
+            }
+            is BleState.Disconnected -> {
+                view?.showMessage("Device disconnected ${state.device.name}")
+            }
+            is BleState.ServicesDiscovered -> {
+                val service = state.services.first { it.id.contains(SERVICE_BUTTON_PRESSED, ignoreCase = true) }
+                view?.showMessage("Service discovered ${service.id}")
+                bluetoothAdapter.discoverCharacteristics(service)
+            }
+            is BleState.CharacteristicsDiscovered -> {
+                val char = state.chars.first { it.id.contains(CHAR_BUTTON_PRESSED, ignoreCase = true) }
+                view?.showMessage("Char discovered ${char.id}")
+                bluetoothAdapter.setNotificationEnabled(char)
+            }
+            is BleState.CharacteristicChanged -> onButtonPressed(state.char)
+        }.let { /* exhaustive */ }
+    }
+
+    private fun onButtonPressed(characteristic: BleCharacteristic) {
+        view?.showMessage("Button pressed!")
     }
 }
 
